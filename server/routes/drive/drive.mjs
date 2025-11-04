021ca9240b948f51cd39618afe5786585bf8338c
Run <runios123@gmail.com>
2025-11-04 17:39:07 +0200

    Cleanup

--- server/routes/drive/drive.mjs
+++ server/routes/drive/drive.mjs
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import { driveGet, fileGet } from "../../controllers/drive/drive.mjs";
 import addRouter from "./add.mjs";
-import propertiesRouter from "./properties.mjs";
 import { deleteDirectory } from "../../controllers/drive/delete.mjs";
 
 const driveRouter = Router();
 driveRouter.use("/add", addRouter);
-driveRouter.use("/properties", propertiesRouter);
 
 driveRouter.delete("/:dirId", deleteDirectory);
 driveRouter.get("/file/{*dirPath}", fileGet);
--- server/routes/index.mjs
+++ server/routes/index.mjs
@@ -1,13 +1,10 @@
 import { Router } from "express";
-import { indexGet, logoutGet } from "../controllers/index.mjs";
 import usersRouter from "./users.mjs";
 import driveRouter from "./drive/drive.mjs";
 import isAuth from "../middleware/auth.mjs";
 
 const indexRouter = Router();
 
-indexRouter.get("/", indexGet);
-indexRouter.get("/logout", logoutGet);
 indexRouter.use("/users", usersRouter);
 indexRouter.use("/drive", isAuth, driveRouter);
 
