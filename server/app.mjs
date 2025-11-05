b9af8d006dde25549e2a57ef42f3ba1464b18efc
Run <runios123@gmail.com>
2025-11-05 13:05:30 +0200

    Update client address to netlify deployed client

--- server/app.mjs
+++ server/app.mjs
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://fileupl0ader.netlify.app/",
     credentials: true,
   })
 );
